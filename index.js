10a7a25421055b3e04fc0260dc0690bd985cfa7a
Aa041997 <144029748+Aa041997@users.noreply.github.com>
2023-09-06 14:49:08 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+document.getElementById('about-btn').addEventListener('click', (e) => {
+    document.getElementById('aboutme').style.display = `block`
+    document.getElementById('home').style.display = `none`
+    document.getElementById('family').style.display = `none`
+    document.getElementById('info-mgmt').style.display = `none`
+
+})
+document.getElementById('family-btn').addEventListener('click', (e) => {
+    document.getElementById('aboutme').style.display = `none`
+    document.getElementById('home').style.display = `none`
+    document.getElementById('family').style.display = `block`
+    document.getElementById('info-mgmt').style.display = `none`
+
+})
+document.getElementById('info-mgmt-btn').addEventListener('click', (e) => {
+    document.getElementById('aboutme').style.display = `none`
+    document.getElementById('home').style.display = `none`
+    document.getElementById('family').style.display = `none`
+    document.getElementById('info-mgmt').style.display = `block`
+
+})
+document.getElementById('home-btn').addEventListener('click', (e) => {
+    document.getElementById('aboutme').style.display = `none`
+    document.getElementById('home').style.display = `block`
+    document.getElementById('family').style.display = `none`
+    document.getElementById('info-mgmt').style.display = `none`
+
+})
+document.getElementById('info-btn2').addEventListener('click', (e) => {
+    document.getElementById('aboutme').style.display = `none`
+    document.getElementById('home').style.display = `none`
+    document.getElementById('family').style.display = `none`
+    document.getElementById('info-mgmt').style.display = `block`
+
+})
+document.getElementById('about-btn2').addEventListener('click', (e) => {
+    document.getElementById('aboutme').style.display = `block`
+    document.getElementById('home').style.display = `none`
+    document.getElementById('family').style.display = `none`
+    document.getElementById('info-mgmt').style.display = `none`
+
+})
+document.getElementById('family-btn2').addEventListener('click', (e) => {
+    document.getElementById('aboutme').style.display = `none`
+    document.getElementById('home').style.display = `none`
+    document.getElementById('family').style.display = `block`
+    document.getElementById('info-mgmt').style.display = `none`
+
+})
